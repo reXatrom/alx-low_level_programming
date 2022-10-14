ad0209d3ef48420cb7a5660d639d2fac37c3f678
reXatrom <ajstylejoshua2837@gmail.com>
2022-10-14 15:16:35 -0700

    task 8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char ls;
+
+	int n;
+
+	ls = 'a';
+	while
+		(n < 10) {
+			putchar(n + '0');
+			n++;
+		}
+	while
+		(ls <= 'f') {
+			putchar(ls);
+			ls++;
+		}
+	putchar('\n');
+	return (0);
+}
