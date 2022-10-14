8b14da80e06bcd3a824c25330dacb16648d45d01
reXatrom <ajstylejoshua2837@gmail.com>
2022-10-14 14:41:33 -0700

    task 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int j;
+
+	for (j = 0; j < 10; j++)
+		printf("%d", j);
+	putchar('\n');
+	return (0);
+}
