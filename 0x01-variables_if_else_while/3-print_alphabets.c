cd625e9f2430f261a34764b00921860fb62c6cdc
reXatrom <ajstylejoshua2837@gmail.com>
2022-10-14 14:08:04 -0700

    task 3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char cl;
+
+	char sl;
+
+	sl = 'a';
+	cl = 'A';
+	while
+		(sl <= 'z')
+		{putchar(sl);
+			sl++;
+		}
+	while
+		(cl <= 'Z')
+		{putchar(cl);
+			cl++;
+		}
+	putchar('\n');
+	return (0);
+}
