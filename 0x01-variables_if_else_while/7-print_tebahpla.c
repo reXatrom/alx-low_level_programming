be7678f5f7b0bcecd7fc6154fb7ef43f716c6a34
reXatrom <ajstylejoshua2837@gmail.com>
2022-10-14 15:02:59 -0700

    task 7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char sl;
+
+	sl = 'z';
+	while
+		(sl >= 'a') {
+			putchar(sl);
+			sl--;
+		}
+	putchar('\n');
+	return (0);
+}
