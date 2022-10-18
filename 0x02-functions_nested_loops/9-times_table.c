ab71bdcac83db4645f506d074793aaa363ef9895
reXatrom <ajstylejoshua2837@gmail.com>
2022-10-18 06:17:54 -0700

    task 9 correction

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -23,8 +23,8 @@ void times_table(void)
 			if (prod <= 9)
 				_putchar(' ');
 			else
-				((prod / 10) + '0');
-			((prod % 10) + '0');
+				_putchar((prod / 10) + '0');
+			_putchar((prod % 10) + '0');
 		}
 		_putchar('\n');
 	}
