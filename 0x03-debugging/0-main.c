46a5f5b90f21f33b01eb5d41c6db8654dd9393f6
reXatrom <ajstylejoshua2837@gmail.com>
2022-10-19 15:03:05 -0700

    task 0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - function that prints if the integer is positive
+ * or negative
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 98;
+	positive_or_negative(i);
+
+	return (0);
+}
