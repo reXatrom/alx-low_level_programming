7d77889332a5f6f913e71fa43648dc5e0d797d74
reXatrom <ajstylejoshua2837@gmail.com>
2022-10-20 04:06:26 -0700

    task 1-3

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main"
+
+/**
+ * mul - multiplication of two integers
+ * @a: first integer
+ * @l: second integer
+ *
+ * Return: result of the multiplication
+ */
+
+int mul(int a, int l)
+{
+	return (a * l);
+}
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - a function that prints the numbers,
+ * from 0 to 9, followed by a new line.
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	int n;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		_putchar((n % 10) + '0');
+	}
+	_putchar('\n');
+}
