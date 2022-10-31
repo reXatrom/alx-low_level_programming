5517160c42c6d0f01aa712de96d29876dd86c594
reXatrom <ajstylejoshua2837@gmail.com>
2022-10-31 15:52:57 -0700

    task 6 correct

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -11,13 +11,12 @@ void print_chessboard(char (*a)[8])
 {
 	int j, k;
 
-	for (j = 0; j < 8, j++)
+	for (j = 0; a[j][7]; j++)
 	{
-		for (k = 0; k < 8, k++)
+		for (k = 0; k < 8; k++)
 		{
 			_putchar(a[j][k]);
 		}
 		_putchar('\n');
 	}
-	return (void);
 }
