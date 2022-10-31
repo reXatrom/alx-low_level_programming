fba4a6fc5490958ea49a088fa91e7ab02dc0f850
reXatrom <ajstylejoshua2837@gmail.com>
2022-10-31 05:48:46 -0700

    task 1 and 2

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _memcpy - a function that copies memory area
+ * @n: function
+ * @src: bytes
+ * @dest: memory area
+ *
+ * Return: a pointer to @dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i = 0;
+
+	while (i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	return (dest);
+}
