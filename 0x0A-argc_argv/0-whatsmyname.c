d9c38592dfb2428b26536836d011440998d5d442
reXatrom <ajstylejoshua2837@gmail.com>
2022-11-04 10:30:20 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - print the name
+ *
+ * @argc: number of parameter
+ *
+ * @argv: value of parameter
+ *
+ * Return: 0
+ */
+
+int main(int argc, char **argv)
+{
+	int i = 0;
+
+	printf("%s\n", argv[i]);
+	return (0);
+}
