537a57d3b7cddce21cb7e511548daa96bbafb880
reXatrom <ajstylejoshua2837@gmail.com>
2023-01-17 04:44:27 -0800

    0x18. C - Dynamic libraries

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+
+/**
+ * add - a function tgat adds
+ * @num1: a parameter
+ * @num2: a oarameter
+ * Return: returns an int
+ */
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
+
+/**
+ * sub - a function that subtracts
+ * @num1: parameter
+ * @num2: paraeter
+ * Return: returns an int
+ */
+int sub(int num1, int num2)
+{
+	return (num1 - num2);
+}
+
+/**
+ * mul - function that multiolies
+ * @num1: parameter
+ * @num2: parameter
+ * Return: returns int
+ */
+int mul(int num1, int num2)
+{
+	return (num1 * num2);
+}
+
+/**
+ * div - a function that divides
+ * @num1: parameter
+ * @num2: parameter
+ * Return: returns integer
+ */
+int div(int num1, int num2)
+{
+	return (num1 / num2);
+}
+
+/**
+ * mod - a function
+ * @num1: parameter
+ * @num2: parameter
+ * Return: returns an integer
+ */
+int mod(int num1, int num2)
+{
+	return (num1 % num2);
+}
